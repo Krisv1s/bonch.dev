ab0f720dfa8d8f80b4cbde9d253812edf5b618e3
Krisv1s <51087951+Krisv1s@users.noreply.github.com>
2021-03-05 09:39:22 +0300

    Create 0.1_work.js

---
+++ 0.1_work.js
@@ -0,0 +1,31 @@
+function bonchHomeWork(num) {
+  for (let i = num; i > 0; i--) {
+    switch (i) {
+      case 5:
+        console.log('пять');
+        break;
+      case 13:
+        console.log('тринадцать');
+        break;
+      case 22:
+        console.log('двадцать два');
+        break;
+      case 35:
+        console.log('тридцать пять');
+        break;
+      case 98:
+        console.log('девяносто восемь');
+        break;
+      default:
+        console.log(i);
+    }
+  }
+}
+
+bonchHomeWork(15);
+
+bonchHomeWork(5);
+
+bonchHomeWork(98);
+
+bonchHomeWork(124);
